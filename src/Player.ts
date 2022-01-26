bdd872c4d89b76ea7fb361f6657e4ded21db54b8
Andreas Caspers <andreas.caspers@digitecgalaxus.ch>
2022-01-26 10:40:04 +0100

    Runter bis 5, immer noch nur paare

--- src/Player.ts
+++ src/Player.ts
@@ -1,5 +1,7 @@
+import { Card, GameState } from "./GameState";
+
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
+  public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     console.log(gameState);
 
     const ourId = gameState.in_action;
@@ -13,16 +15,15 @@ export class Player {
       betCallback(0);
     }
 
-
     console.error(gameState);
   }
 
-  doWeHaveGoodCards = (cards: any) => {
+  doWeHaveGoodCards = (cards: Card[]) => {
     const card1 = cards[0];
     const card2 = cards[1];
 
     if (card1.rank === card2.rank) {
-      if (card1.rank === 'A' || card1.rank === 'K' || card1.rank === 'Q' || card1.rank === 'J') {
+      if (card1.rank === 'A' || card1.rank === 'K' || card1.rank === 'Q' || card1.rank === 'J' || card1.rank === '10' || card1.rank === '9' || card1.rank === '8' || card1.rank === '7' || card1.rank === '6' || card1.rank === '5' ) {
         return true;
       }
     }
