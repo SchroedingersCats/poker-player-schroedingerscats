7cd5f0c5e913a2418f375093ec507e9af30b7960
Andreas Caspers <andreas.caspers@digitecgalaxus.ch>
2022-01-26 11:17:13 +0100

    Moeny returned

--- src/Player.ts
+++ src/Player.ts
@@ -9,34 +9,30 @@ export class Player {
 
     console.log(`We are ${ourPlayer}`);
 
-    if (this.doWeHaveGoodCards(ourPlayer.hole_cards)) {
-      betCallback(50000);
-    } else {
-      betCallback(0);
-    }
+    betCallback(this.howManyMoneyShouldWeSet(ourPlayer.hole_cards));
 
     console.error(gameState);
   }
 
-  doWeHaveGoodCards = (cards: Card[]) => {
+  howManyMoneyShouldWeSet = (cards: Card[]) => {
     const card1 = cards[0];
     const card2 = cards[1];
 
     if (card1.rank === card2.rank) {
       if (card1.rank === 'A' || card1.rank === 'K' || card1.rank === 'Q' || card1.rank === 'J' || card1.rank === '10' || card1.rank === '9' || card1.rank === '8' || card1.rank === '7' || card1.rank === '6' || card1.rank === '5') {
-        return true;
+        return 5000;
       }
     }
 
     if (card1.rank === 'A' && ['K', 'Q', 'J', '10'].indexOf(card2.rank) !== -1) {
-      return true;
+      return 5000;
     }
 
     if (card2.rank === 'A' && ['K', 'Q', 'J', '10'].indexOf(card1.rank) !== -1) {
-      return true;
+      return 5000;
     }
 
-    return false;
+    return 0;
   }
 
   public showdown(gameState: any): void {
