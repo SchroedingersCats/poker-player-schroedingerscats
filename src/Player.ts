19dd1f0543de759a022daf48b5a2d5e2c08dfe2e
Andreas Caspers <andreas.caspers@digitecgalaxus.ch>
2022-01-26 10:24:31 +0100

    First try with simple all-in logic

--- src/Player.ts
+++ src/Player.ts
@@ -1,10 +1,38 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(50000);
+    console.log(gameState);
+
+    const ourId = gameState.in_action;
+    const ourPlayer = gameState.players[ourId];
+
+    console.log(`We are ${ourPlayer}`);
+
+    if (this.doWeHaveGoodCards(ourPlayer.hole_cards)) {
+      betCallback(50000);
+    } else {
+      betCallback(0);
+    }
+
+
     console.error(gameState);
   }
 
-  public showdown(gameState: any): void {}
+  doWeHaveGoodCards = (cards: any) => {
+    const card1 = cards[0];
+    const card2 = cards[1];
+
+    if (card1.rank === card2.rank) {
+      if (card1.rank === 'A' || card1.rank === 'K' || card1.rank === 'Q' || card1.rank === 'J') {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public showdown(gameState: any): void {
+
+  }
 }
 
 export default Player;
