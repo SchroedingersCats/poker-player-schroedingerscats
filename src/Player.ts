f21b1e4dd0e9744e4268b31fd31288157c130b35
Sven Petroll <sven.petroll@digitecgalaxus.ch>
2022-01-26 09:43:38 +0100

    All-in

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,10 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+    betCallback(50000);
+    console.error(gameState);
   }
 
-  public showdown(gameState: any): void {
-
-  }
-};
+  public showdown(gameState: any): void {}
+}
 
 export default Player;
